53669ce2b3411fd0faae4a4d1eeb9f09890fac29
unknown <you@example.com>
2022-08-02 21:30:35 +0300

    Initial commit

---
+++ src/main/java/tests/LoginWithTestNgParameters.java
@@ -0,0 +1,26 @@
+package tests;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+import utils.BaseTest;
+
+
+public class LoginWithTestNgParameters extends BaseTest {
+
+	@Parameters({"user", "pass"})
+	@Test
+	public void loginWithParameters(String user, String pass) throws InterruptedException {
+		
+		page.navMenu.navigateTo(page.navMenu.loginLink);
+		page.loginPage.loginInApp(user, pass);
+		assertTrue(page.loginPage.sucessloginMessage.isDisplayed());
+		page.loginPage.logoutFromApp();
+		
+	}
+	
+	
+}
